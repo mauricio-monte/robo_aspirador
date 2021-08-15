449ddd594d99d5e5d608ce59daa4cff8ef4bc5ba
mauricio-monte <mauricio.monte@ccc.ufcg.edu.br>
2021-08-14 22:56:24 -0300

    Adiciona inicialização do cenário e execução básica do cenário

--- src/cenario.py
+++ src/cenario.py
@@ -1,43 +1,75 @@
 import numpy as np
 
+from exibir_estado import imprime_estado_simulacao
 
 class Sala:
     vazio = 0
-    obstáculo = 1
+    obstaculo = 1
     sujeira = 2
 
-    def __init__(self, dimensoes_matriz, lista_obstaculos, hot_spots_sujeira, aspirador, posicao_aspirador, posicao_base_recarregamento):
-        self.M = dimensoes_matriz[0]
-        self.N = dimensoes_matriz[1]
-        self.piso = np.array([self.M, self.N], int)
-        self.hot_spots_sujeira = np.array(hot_spots_sujeira, int)  # onde há sujeira
-        self.pos_aspirador = (posicao_aspirador[0], posicao_aspirador[1])
-        self.pos_base = (posicao_base_recarregamento[0], posicao_base_recarregamento[1])
+    def __init__(self, tamanho_sala, lista_obstaculos, hot_spots_sujeira, aspirador, posicao_aspirador, posicao_base_recarregamento):
+        self.piso = np.zeros(list(tamanho_sala), int)
+        self.posicao_agente = list(posicao_aspirador)
+        self.posicao_base_carregamento = list(posicao_base_recarregamento)
+        self.aspirador = aspirador
+        self.lista_obstaculos = lista_obstaculos
+        self.hot_spots_sujeira = hot_spots_sujeira
 
-    # Escreva seu código aqui levando em conta que:
-    # o piso recebe valor vazio em todo lugar e
-    # você deve colocar obstáculos nas coordenadas recebidas no 2o parâmetro do construtor
-    # a sujeira deve ser atualizada no método step() pois, mesmo quando o agente limpa,
-    # a sujeira deverá reaparecer com certa probabilidade
+    def recuperar_estado_piso(self, coordenada):
+        """Mostra o que em uma célula da sala em uma determinada posição"""
+        x = coordenada[0]
+        y = coordenada[1]
+        return self.piso[x][y]
 
-    def step(self):  # atualiza sujeira, realiza a interação do agente-ambiente
-        # Escreva seu código aqui levando em conta o pseudo-código para
-        # adicionar sujeira com maior probabilidade em certos locais:
+    def adiciona_sujeira(self, coordenada):
+        x = coordenada[0]
+        y = coordenada[1]
+        # n = np.random.random_integers(0, 100)
 
-        # atualização da sujeira
-        # for i até dim M
-        #  for j até dim N
-        #      r = rand(0,1)
-        #      se (i,j) pertence a hot_spots_sujeira
-        #      então
-        #            se r < 0.1 então piso(i,j) = sujeira
-        #      senão se r < 0.01 então piso(i,j) = sujeira
+        # if coordenada in self.hot_spots_sujeira:
+        #     if n <= 30:
+        #         self.piso[x][y] = 2
+        # else:
+        #     if n <= 5:
+        #         self.piso[x][y] = 2
 
-        # chamar o programa do agente e atualiza suas coordenadas a partir da
-        # ação recomendada pelo próprio agente
-        # imprimir o estado do ambiente
-        # imprimir o estado do Agente aspirador
-        pass
+        self.piso[x][y] = 2
 
-    def run(self, N):  # chama step N vezes para simular o agente e o seu ambiente
-        pass
+    def remove_sujeira(self, coordenada):
+        x = coordenada[0]
+        y = coordenada[1]
+        self.piso[x][y] = 0
+
+    def percept(self, agent_position):
+        """Mostra para o agente o estado do piso na posição dele"""
+        return self.recuperar_estado_piso(agent_position)
+
+    def step(self):
+        acao_agente = self.aspirador.program(self.percept(self.posicao_agente))
+        print(acao_agente)
+        self.execute_action(self.aspirador, acao_agente)
+
+    def run(self, steps=50):  # chama step N vezes para simular o agente e o seu ambiente
+        for step in range(steps):
+            print('\nxxxxxxxxxxxxxxxxxxxx Step ', step, ' xxxxxxxxxxxxxxxxxxxx')
+            self.step()
+            print('Agent Position: ', self.posicao_agente)
+            print(f'Bateria do Agente: {self.aspirador.bateria}')
+            imprime_estado_simulacao(
+                self.piso, self.aspirador.piso, self.posicao_agente,
+                self.posicao_base_carregamento, self.hot_spots_sujeira,
+                self.lista_obstaculos
+            )
+
+    def execute_action(self, agent, action):
+        '''Changes the state of the environment based on what the agent does.'''
+
+        if action == "walk":
+            self.posicao_agente = agent.update_position()
+
+        elif action == "clean":
+            estado_do_piso = self.recuperar_estado_piso(agent.location)
+            if estado_do_piso == self.sujeira:
+                self.remove_sujeira(agent.location)
+                agent.clean(estado_do_piso)
+            
