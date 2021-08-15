c526bfce0b8624ecca486ed7f8b44c0e2f6c4bf9
mauricio-monte <mauricio.monte@ccc.ufcg.edu.br>
2021-08-14 22:54:54 -0300

    Adiciona inicialização do aspirador e função de andar (não funciona ainda)

--- src/aspirador.py
+++ src/aspirador.py
@@ -1,24 +1,61 @@
 import numpy as np
 
+PISO_LIMPO = 0
+OBSTACULO = 1
+PISO_SUJO = 2
 
 class Aspirador:
-    actions = ["frente", "esq", "dir", "tras", "aspirar", "recarregar"]
-    status_percepção = ["sujo", "vazio", "obstáculo"]  # diz respeito à celula imediatamente à frente do agente se obstáculo
+    location = [0, 0]
+    
+    def __init__(self, bateria, tamanho_sala):
+        self.bateria = bateria
+        self.piso = np.zeros(list(tamanho_sala), int)
+    
+    def program(self, estado_piso):
+        if estado_piso == PISO_SUJO:
+            return 'clean'
+        return 'walk'
 
-    def __init__(self, energia_aspirador, M, N):
-        self.energia = energia_aspirador
-        # consulta o ambiente para obter as coordenadas
-        modelo_ambiente = np.array([M, N], int)  # registra as experiências do robô.
-        # no modelo é guardado: contador de sujeira (0...MAX) ou obstáculo (-1)
+    def update_position(self):
+        y = self.location[0]
+        x = self.location[1]
+        direcao = "direita"
         
-        # definida a partir do contador de sujeira e da posição atual.
-        avaliação_heurística = np.array([M, N], int)
+        if direcao == "direita":
+            if y < len(self.piso[0]) - 1:
+                self.location[1] += 1
+            else:
+                direcao = "baixo"
 
-    def update_posição(nova_posicao):
-        pass
+        elif direcao == "esquerda": 
+            if  x == 0:
+                self.location[1] -= 1
+            else:
+                direcao = "cima"
 
-    def action_agent_program(percepcao):
-        pass
+        elif direcao == "cima": 
+            if y == 0:
+                self.location[0] -= 1
+            else:
+                direcao = "direita"
+
+        elif direcao == "baixo": 
+            if y < len(self.piso) - 1:
+                self.location[0] += 1
+            else:
+                direcao = "esquerda"
+
+        self.bateria -= 1
+        return self.location
+
+    def clean(self, estado_do_piso):
+        if estado_do_piso == PISO_SUJO:
+            self.bateria -= 5
+            return True
+        return False
+
+    def get_bateria(self):
+        return self.bateria
 
     # realizar busca heurística usando a avaliação heurística, o modelo do ambiente e a percepção corrente.
     # considerar que ele deve retornar à base quando a bateria estiver crítica
