a368554a139c442aba91e2ceefb7228d0a8b5d78
mauricio-monte <mauricio.monte@ccc.ufcg.edu.br>
2021-08-26 22:06:37 -0300

    Adiciona testes de desvio de obstáculos

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,14 @@
-from testes import teste_professor, teste_nosso
+from testes import (
+    teste_professor,
+    teste_nosso,
+    teste_obstaculo_meio_linha_horizontal,
+    teste_obstaculo_inicio_linha_horizontal,
+    teste_obstaculo_final_linha_horizontal
+)
 
 
 def main():
-    teste_nosso()
+    teste_obstaculo_meio_linha_horizontal()
 
 
 if __name__ == '__main__':
--- src/testes.py
+++ src/testes.py
@@ -20,12 +20,46 @@ def teste_professor():
 def teste_nosso():
     tamanho_sala = (10, 10)
     bateria = 10000
-    array_de_obstaculos =[[1, 9]] # [[0, 1], [3, 0]]
+    array_de_obstaculos = [[1, 9]] # [[0, 1], [3, 0]]
     hot_spots_sujeira = [[0, 2], [3, 3]]
-
     aspirador = Aspirador(bateria, tamanho_sala)
 
-    # Perguntar para o professor sobre as coordenadas usadas para inicializar o ambiente: as tuplas são do formato é (x, y) ou (linha, coluna)?
+    sala = Sala(tamanho_sala, array_de_obstaculos, hot_spots_sujeira, aspirador, (0, 0), (4, 5))
+
+    sala.run(400)
+
+def teste_obstaculo_inicio_linha_horizontal():
+    tamanho_sala = (10, 10)
+    bateria = 10000
+    array_de_obstaculos = [[1, 9]]
+    rota_desvio = [(0,8), (1,8)] # Essa é a rota que se espera que seja retornada pela bfs
+    hot_spots_sujeira = [[0, 2], [3, 3]]
+    aspirador = Aspirador(bateria, tamanho_sala)
+
+    sala = Sala(tamanho_sala, array_de_obstaculos, hot_spots_sujeira, aspirador, (0, 0), (4, 5))
+
+    sala.run(400)
+
+def teste_obstaculo_meio_linha_horizontal():
+    tamanho_sala = (10, 10)
+    bateria = 10000
+    array_de_obstaculos = [[0, 5]]
+    rota_desvio = [(1, 4), (1, 5), (1, 6), (0, 6)] # Essa é a rota que se espera que seja retornada pela bfs
+    hot_spots_sujeira = [[0, 2], [3, 3]]
+    aspirador = Aspirador(bateria, tamanho_sala)
+
+    sala = Sala(tamanho_sala, array_de_obstaculos, hot_spots_sujeira, aspirador, (0, 0), (4, 5))
+
+    sala.run(400)
+
+def teste_obstaculo_final_linha_horizontal():
+    tamanho_sala = (10, 10)
+    bateria = 10000
+    array_de_obstaculos = [[0, 9]] 
+    rota_desvio = [(1,8), (1,9)] # Essa é a rota que se espera que seja retornada pela bfs
+    hot_spots_sujeira = [[0, 2], [3, 3]]
+    aspirador = Aspirador(bateria, tamanho_sala)
+
     sala = Sala(tamanho_sala, array_de_obstaculos, hot_spots_sujeira, aspirador, (0, 0), (4, 5))
 
     sala.run(400)
